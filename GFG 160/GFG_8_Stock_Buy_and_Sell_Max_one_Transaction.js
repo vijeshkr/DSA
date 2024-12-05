95ed5902498c31c0b3a1bb6b3a8fd3c8058ec0d0
Vijesh KR <vijeshkr17@gmail.com>
2024-12-05 22:23:50 +0530

    GFG 8th problem Stock buy and sell max one transaction allowed

---
+++ GFG 160/GFG_8_Stock_Buy_and_Sell_Max_one_Transaction.js
@@ -0,0 +1,31 @@
+/**
+ * Given an array prices[] of length n, representing the prices of the stocks on different days. 
+ * The task is to find the maximum profit possible by buying and selling the stocks on different days when at most one transaction is allowed. 
+ * Here one transaction means 1 buy + 1 Sell. If it is not possible to make a profit then return 0.
+ * Note: Stock must be bought before being sold.
+ */
+
+class Solution {
+    // Function to find the maximum profit using the two-pointer method.
+    maximumProfit(prices) {
+        let left = 0;
+        let right = 1;
+        let maxProfit = 0;
+
+        while (right < prices.length) {
+            if (prices[right] < prices[left]) {
+                left = right;
+            } else {
+                let profit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            right++;
+        }
+
+        return maxProfit;
+    }
+}
+
+// Time Complexity O(n)
+// Space Complexity O(n)
+
